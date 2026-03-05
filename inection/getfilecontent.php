790fee2bc39611b8cdfc4ce1ba19185ec6e40989
AlexandraSpeshilova <spesilovasasa13@gmail.com>
2026-03-05 22:04:47 +0500

    создание файла getfilecontent и получение данных пользователя из бд

---
+++ inection/getfilecontent.php
@@ -0,0 +1,4 @@
+<?
+    $getcon = file_get_contents('../settings/connect_datebase.php');
+    echo htmlspecialchars($getcon);
+?>
---
+++ inection/getuser.php
@@ -0,0 +1,9 @@
+<?php
+include("../settings/connect_datebase.php");
+$sql = "SELECT * FROM users";
+$result = $mysqli->query($sql);
+while($row = $result->fetch_assoc()) {
+    echo "Логин: " . htmlspecialchars($row['login']) . "\n";
+    echo "Пароль: " . htmlspecialchars($row['password']) . "\n" ;
+}
+?>
