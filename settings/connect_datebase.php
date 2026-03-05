b3ac823e605c5b0a552ef53cd439b81690415f42
AlexandraSpeshilova <spesilovasasa13@gmail.com>
2026-03-05 21:49:44 +0500

    создание файлов phpinfo и getdir

--- settings/connect_datebase.php
+++ settings/connect_datebase.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new mysqli('127.0.0.1', 'root', '', 'security_permaviat');
+	$mysqli = new mysqli('127.0.0.1', 'root', '', 'aviksecur');
 	
 	function getClientIP() {
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
